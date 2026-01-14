1b600ea3f9dbba9b1c97b380da095ee1170eb07a
drmbmk20-creator <dr.mbmk20@gmail.com>
2026-01-15 01:25:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,37 @@
-self.addEventListener('fetch', (e) => {
-   
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
+const CACHE_NAME = 'mbmk-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700;800&family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap',
+    'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
